199d2aa94e3049f7e62bb7d1dfcedb4e1c1ceda5
VanstySanic <wzy129529138@gmail.com>
2025-11-27 15:29:06 +0800

    feat: alpha blend feature processing: alpha blend proxy

---
+++ Plugins/Vansty/Core/VSPluginsCore/Source/VSPluginsCore/Private/Classes/Features/VSAlphaBlendFeature.cpp
@@ -0,0 +1,162 @@
+﻿// Copyright VanstySanic. All Rights Reserved.
+
+#include "Classes/Features/VSAlphaBlendFeature.h"
+#include "Classes/VSTickableObject.h"
+
+UVSAlphaBlendFeature::UVSAlphaBlendFeature(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UVSAlphaBlendFeature::Initialize_Implementation()
+{
+	Super::Initialize_Implementation();
+
+	SetAlpha(DefaultAlpha);
+}
+
+void UVSAlphaBlendFeature::Uninitialize_Implementation()
+{
+	SetAlpha(DefaultAlpha);
+	
+	Super::Uninitialize_Implementation();
+}
+
+void UVSAlphaBlendFeature::BeginPlay_Implementation()
+{
+	Super::BeginPlay_Implementation();
+	
+	if (bDefaultAutoUpdate)
+	{
+		SetAutoUpdate(true);
+	}
+}
+
+void UVSAlphaBlendFeature::EndPlay_Implementation()
+{
+	SetAutoUpdate(false);
+	
+	Super::EndPlay_Implementation();
+}
+
+void UVSAlphaBlendFeature::Tick_Implementation(float DeltaTime)
+{
+	Super::Tick_Implementation(DeltaTime);
+
+	if (bIsAutoUpdating)
+	{
+		Update(DeltaTime);
+	}
+}
+
+bool UVSAlphaBlendFeature::HasFinished() const
+{
+	return (Direction == ETimelineDirection::Forward && UpdatedTime >= AlphaBlendArgs.BlendTime) || (Direction == ETimelineDirection::Backward && UpdatedTime <= 0.f);
+}
+
+void UVSAlphaBlendFeature::SetAutoUpdate(bool bEnabled)
+{
+	if (bIsAutoUpdating == bEnabled) return;
+	bIsAutoUpdating = bEnabled;
+}
+
+void UVSAlphaBlendFeature::SetTime(float InTime)
+{
+	const bool PrevHasFinished = HasFinished();
+	UpdatedTime = FMath::Clamp(InTime, 0.f, AlphaBlendArgs.BlendTime);
+	const float LinerAlpha = FMath::GetMappedRangeValueClamped(FVector2D(0.f, AlphaBlendArgs.BlendTime), FVector2D(0.f, 1.f), UpdatedTime);
+	Alpha = FAlphaBlend::AlphaToBlendOption(LinerAlpha,AlphaBlendArgs.BlendOption, AlphaBlendArgs.CustomCurve);
+
+	OnUpdated_Native.Broadcast(this, Alpha, UpdatedTime);
+	OnUpdated.Broadcast(this, Alpha, UpdatedTime);
+	
+	if (!PrevHasFinished && HasFinished())
+	{
+		DoFinishedLogic();
+	}
+}
+
+void UVSAlphaBlendFeature::SetAlpha(float InAlpha)
+{
+	const bool PrevHasFinished = HasFinished();
+
+	Alpha = FMath::Clamp(InAlpha, 0.f, 1.f);
+	
+	float Low = 0.f, High = 1.f;
+	while (Low < High)
+	{
+		float Mid = 0.5f * (Low + High);
+		float Test = FAlphaBlend::AlphaToBlendOption(Mid, AlphaBlendArgs.BlendOption, AlphaBlendArgs.CustomCurve);
+		if (FMath::IsNearlyEqual(Alpha, Test, 0.001f))
+		{
+			break;
+		}
+		if (Test < Alpha)
+		{
+			Low = Mid;
+		}
+		else
+		{
+			High = Mid;
+		}
+	}
+	const float LinerAlpha = 0.5f * (Low + High);
+	UpdatedTime = FMath::Clamp(LinerAlpha * AlphaBlendArgs.BlendTime, 0.f, AlphaBlendArgs.BlendTime);
+
+	OnUpdated_Native.Broadcast(this, Alpha, UpdatedTime);
+	OnUpdated.Broadcast(this, Alpha, UpdatedTime);
+	
+	if (!PrevHasFinished && HasFinished())
+	{
+		DoFinishedLogic();
+	}
+}
+
+void UVSAlphaBlendFeature::SetUpdateTimeInterval(float Interval)
+{
+	if (TickProxy)
+	{
+		TickProxy->PrimaryObjectTick.TickInterval = Interval;
+	}
+}
+
+void UVSAlphaBlendFeature::DoFinishedLogic()
+{
+	if (bStopAutoUpdateWhenFinished)
+	{
+		SetAutoUpdate(false);
+	}
+	
+	OnFinished_Native.Broadcast(this, Alpha, UpdatedTime);
+	OnFinished.Broadcast(this, Alpha, UpdatedTime);
+}
+
+void UVSAlphaBlendFeature::Update(float DeltaTime)
+{
+	const float Time = FMath::Clamp(UpdatedTime + DeltaTime * (Direction == ETimelineDirection::Forward ? 1.f : -1.f), 0.f, AlphaBlendArgs.BlendTime);
+	SetTime(Time);
+}
+
+void UVSAlphaBlendFeature::ReverseDirection()
+{
+	Direction = Direction == ETimelineDirection::Forward ? ETimelineDirection::Backward : ETimelineDirection::Forward;
+}
+
+void UVSAlphaBlendFeature::SetAlphaBlendArgs(const FAlphaBlendArgs& NewArgs)
+{
+	AlphaBlendArgs = NewArgs;
+	SetAlpha(Alpha);
+}
+
+
+/** ------------------------------------------------------------------------- **/
+
+
+UVSAlphaBlendProxy* UVSAlphaBlendProxy::CreateAlphaBlendProxy(UObject* Outer)
+{
+	if (!Outer || !Outer->GetWorld()) return nullptr;
+	UVSAlphaBlendProxy* Proxy = NewObject<UVSAlphaBlendProxy>(Outer);
+	
+	
+	return Proxy;
+}
---
+++ Plugins/Vansty/Core/VSPluginsCore/Source/VSPluginsCore/Public/Classes/Features/VSAlphaBlendFeature.h
@@ -0,0 +1,167 @@
+﻿// Copyright VanstySanic. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Classes/VSObjectFeature.h"
+#include "Components/TimelineComponent.h"
+#include "VSAlphaBlendFeature.generated.h"
+
+/**
+ * 
+ */
+UCLASS(DisplayName = "VS.Feature.AlphaBlend")
+class VSPLUGINSCORE_API UVSAlphaBlendFeature : public UVSObjectFeature
+{
+	GENERATED_UCLASS_BODY()
+	
+	DECLARE_MULTICAST_DELEGATE_ThreeParams(FAlphaBlendDelegate, UVSAlphaBlendFeature* /** Feature */, float /** Alpha */, float /** CurrentTime */);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAlphaBlendEvent, UVSAlphaBlendFeature*, Feature, float, Alpha, float, CurrentTime);
+
+protected:
+	//~ Begin UVSObjectFeature Interface.
+	virtual void Initialize_Implementation() override;
+	virtual void Uninitialize_Implementation() override;
+	virtual void BeginPlay_Implementation() override;
+	virtual void EndPlay_Implementation() override;
+	virtual void Tick_Implementation(float DeltaTime) override;
+	//~ End UVSObjectFeature Interface.
+
+	/** Get current alpha blend args. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	FAlphaBlendArgs GetAlphaBlendArgs() const { return AlphaBlendArgs; }
+
+	/** Set new alpha blend args and keep current alpha. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend", meta = (AutoCreateRefTerm = "NewArgs"))
+	void SetAlphaBlendArgs(const FAlphaBlendArgs& NewArgs);
+
+	/** Get current alpha. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	float GetAlpha() const { return Alpha; }
+
+	/** Whether alpha blend has finished. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	bool HasFinished() const;
+
+	/** Enable or disable auto updating. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void SetAutoUpdate(bool bEnabled);
+
+	/** Whether alpha blend is auto updating. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	bool IsAutoUpdating() const { return bIsAutoUpdating; }
+
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	float GetTime() const { return UpdatedTime; }
+	
+	/** Set time and update alpha accordingly. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void SetTime(float InTime);
+
+	/** Set alpha and update time accordingly. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void SetAlpha(float InAlpha);
+
+	/** Update alpha blend manually with DeltaTime. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void Update(float DeltaTime);
+	
+	/** Reverse current playback direction. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void ReverseDirection();
+
+	/** Set interval between automatic updates. */
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void SetUpdateTimeInterval(float Interval = 0.f);
+
+protected:
+	/** Execute logic when blend is finished. */
+	UFUNCTION(Blueprintable, Category = "AlphaBlend")
+	void DoFinishedLogic();
+	
+public:
+	FAlphaBlendDelegate OnUpdated_Native;
+	FAlphaBlendDelegate OnFinished_Native;
+	
+	UPROPERTY(BlueprintReadOnly, BlueprintAssignable)
+	FAlphaBlendEvent OnUpdated;
+
+	UPROPERTY(BlueprintReadOnly, BlueprintAssignable)
+	FAlphaBlendEvent OnFinished;
+	
+public:
+	UPROPERTY(EditAnywhere, Category = "AlphaBlend", meta = (ClampMin = "0.f", ClampMax = "1.f"))
+	float DefaultAlpha = 0.f;
+	
+	UPROPERTY(EditAnywhere, Category = "AlphaBlend")
+	uint8 bDefaultAutoUpdate : 1;
+
+	UPROPERTY(EditAnywhere, Category = "AlphaBlend")
+	uint8 bStopAutoUpdateWhenFinished : 1;
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "AlphaBlend")
+	TEnumAsByte<ETimelineDirection::Type> Direction = ETimelineDirection::Forward;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "AlphaBlend")
+	FAlphaBlendArgs AlphaBlendArgs;
+
+	uint8 bIsAutoUpdating : 1;
+	float Alpha = 0.f;
+	float UpdatedTime = 0.f;
+};
+
+
+/** ------------------------------------------------------------------------- **/
+
+
+USTRUCT()
+struct FVSAlphaBlendProxyParams
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FAlphaBlendArgs AlphaBlendArgs;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 LoopCount = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 LoopTimeInterval : 1;
+};
+
+UCLASS(BlueprintType)
+class UVSAlphaBlendProxy : public UObject
+{
+	GENERATED_UCLASS_BODY()
+	
+	DECLARE_MULTICAST_DELEGATE_TwoParams(FAlphaBlendProxyDelegate, UVSAlphaBlendFeature* /** Feature */, float /** Alpha */);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FAlphaBlendProxyEvent, UVSAlphaBlendFeature*, Feature, float, Alpha);
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend", meta = (DefaultToSelf = "Outer"))
+	static UVSAlphaBlendProxy* CreateAlphaBlendProxy(UObject* Outer);
+
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void StopAtLoopEnd();
+	
+	UFUNCTION(BlueprintCallable, Category = "AlphaBlend")
+	void StopImmediately();
+	
+public:
+	FAlphaBlendProxyDelegate OnUpdated_Native;
+	
+	UPROPERTY(BlueprintReadOnly, BlueprintAssignable)
+	FAlphaBlendProxyEvent OnUpdated;
+
+	UPROPERTY(BlueprintReadOnly, BlueprintAssignable)
+	FAlphaBlendProxyEvent OnFinished;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UVSAlphaBlendFeature> AlphaBlendFeature;
+
+	int32 LoopCount = 1;
+
+};
